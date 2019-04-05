eaa27a7f43adc40e831f94967835944cda086b15
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2019-04-05 14:17:14 -0400

    added express server for production

---
+++ production-server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const proxy = require('http-proxy-middleware')
+const port = process.env.PORT || 3001
+
+// body parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(require('body-parser').text())
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+//http://localhost:1337
+app.use(proxy('/api', { target: 'http://localhost:1337' }))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+  next()
+})
+
+app.listen(port, () => {
+  console.log(`Doin' haxor stuff on port ${port}`)
+})
